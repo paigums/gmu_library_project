c353ee24cba95a0852af7d4255c7e1faf153c8da
Paige Murphy <pmurph11@gmu.edu>
2014-04-22 19:15:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmuLibraryProject::Application.config.secret_key_base = 'f0ae33fd3b512fb35eb8fbf8ee55f86ba2f6c475ce35e418d731d2b334a4ba79500c69d55b3056bc914422f24e896ceddf5b7cd983339d35cb3a92bb8956b1e7'
