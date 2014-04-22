bbb4057ef2498774be2e183eb0da947ccc56d648
Paige Murphy <pmurph11@gmu.edu>
2014-04-22 19:27:25 -0400

    Refreshing Repo

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'cc525a5b97354a2f920e06b0442e2cc2f7a47378cb78a2c53817be941844ad92f25b0831ee4ef3f581402467cf36c0af9cad349e885640f67c4eb20e2c0af4bd'
