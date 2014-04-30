a509f295f780fc29137b85c9be2fdceb54d552e5
Paige Murphy <pmurph11@gmu.edu>
2014-04-29 21:57:34 -0400

    Applying Lab 9 work to Library

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,11 +1,19 @@
 Library::Application.routes.draw do
 
   get "/users" => "users#index", as: 'users'
+  get "/users/new" => "users#new", as: 'new_user'
   get "/users/:id" => "users#show", as: 'user'
-  
-  get "/books" => "books#index", as: 'books'
-  get "/books/:id" => "books#show", as: 'book'
-  
+  post "/users" => "users#create"
+
+  resources :books
+
+ # get "/books" => "books#index", as: 'books'
+ # get "/books/new" => "books#new", as: 'new_book'
+ # get "/books/:id" => "books#show", as: 'book'
+ # post "/books" => "books#create"
+ # get "/books/:id/edit" => "books#edit", as: 'edit_book'
+ # patch "/books/:id" => "books#update"
+ # delete "/books/:id" => "books#destroy"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
