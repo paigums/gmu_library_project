c1a180908e631f9dc41fdd251c802e0d9997210d
rcolema9 <coleman_richard2@bah.com>
2014-05-24 15:18:54 -0400

    Update reservations_controller.rb

--- library/app/controllers/reservations_controller.rb
+++ library/app/controllers/reservations_controller.rb
@@ -10,7 +10,10 @@ class ReservationsController < ApplicationController
 	end
 
 	def create
-	  @reservation = @book.reservations.new(reservation_params)
+	  #@reservation = @book.reservations.new(reservation_params)
+  	  now = Time.new
+	  #user = User.find_by(id: session[:user_id])
+	  @reservation = @book.reservations.new({:reserved_on => now, :due_on => now+7.days, :user => current_user})
 	  if @reservation.save
 	    redirect_to book_reservations_path(@book), notice: 'Reserved!'
 	  else
@@ -25,8 +28,8 @@ class ReservationsController < ApplicationController
 	  @book = Book.find(params[:book_id])
 	end
 
-	def reservation_params
-	  params.require(:reservation).permit(:reserved_on, :due_on)
-	end
+	#def reservation_params
+	#  params.require(:reservation).permit(:reserved_on, :due_on)
+	#end
 
 end
